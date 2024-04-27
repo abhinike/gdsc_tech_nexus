8422ea459dde36dcb4457ba84e4e146c876fcc03
varshaislur <varshaislur@gmail.com>
2024-04-27 23:45:25 +0530

    basic ui created

--- lib/Presentation/MarksPrediction/marks_prediction_screen.dart
+++ lib/Presentation/MarksPrediction/marks_prediction_screen.dart
@@ -1,43 +1,68 @@
 import 'dart:convert';
-import 'package:http/http.dart';
-import 'package:http/http.dart ' as http;
-
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class MarksPredictionScreen extends StatefulWidget {
-  const MarksPredictionScreen({super.key});
+  const MarksPredictionScreen({Key? key}) : super(key: key);
 
   @override
-  State<MarksPredictionScreen> createState() => _MarksPredictionScreenState();
+  _MarksPredictionScreenState createState() => _MarksPredictionScreenState();
 }
 
 class _MarksPredictionScreenState extends State<MarksPredictionScreen> {
-  Future<void> getMarks() async {
-    Map<String, dynamic> data = {"study_hours": 5};
-    var encodedData = jsonEncode(data);
+  String image = "";
+  double marks = 0;
+  bool isLoading = false;
 
-    final response = await http.post(
-      Uri.parse("https://demo-krh2.onrender.com/predict"),
-      headers: {"Content-Type": "application/json"},
-      body: encodedData,
-    );
-    print(response.statusCode.toString());
-    if (response.statusCode == 200) {
-      var decodedData = jsonDecode(response.body);
-      print('Response data: $decodedData');
+  Future<void> getMarks() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final response = await http.post(
+        Uri.parse("https://demo-krh2.onrender.com/predict"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"study_hours": 5}),
+      );
+
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body);
+        setState(() {
+          image = decodedData['image_url'];
+          marks = decodedData['predicted_marks'];
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('Error: $e');
+
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ElevatedButton(onPressed: getMarks, child: Text("marks"))
+          ElevatedButton(
+            onPressed: isLoading ? null : getMarks,
+            child: Text("Get Marks"),
+          ),
+          SizedBox(height: 16),
+          if (isLoading)
+            CircularProgressIndicator()
+          else
+            Text("Predicted Marks: $marks"),
+          SizedBox(height: 16),
+          if (image.isNotEmpty) Image.network(image),
         ],
-
       ),
     );
   }
